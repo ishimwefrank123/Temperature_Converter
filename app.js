1af914af8973982e29412b12c99a78f79d41b9c1
ishimwefrank123 <ishimwefrank147@gmail.com>
2024-09-30 11:54:56 +0200

    display the temperature in different degree when the use input in one field

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let celciusInput = document.querySelector('#celcius > input');
+
 let fahrenheitInput = document.querySelector('#fahrenheit > input');
+
 let kelvinInput = document.querySelector('#kelvin > input');
 
 let btn = document.querySelector('.button button');
@@ -8,3 +10,12 @@ let btn = document.querySelector('.button button');
 function roundNumber(number){
     return Math.round(number*100)/100;
 }
+
+celciusInput.addEventListener('input', function(){
+    let cTemp = parseFloat(celciusInput.value);
+    let fTemp = (cTemp*(9/5)) + 32;
+    let kTemp = cTemp + 273.15;
+
+    fahrenheitInput.value = roundNumber(fTemp)
+    kelvinInput.value = roundNumber(kTemp)
+})
